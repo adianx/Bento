553ffdc0bd69269a12ead4be2690c465cceebeb5
Aditya Pant <65610586+adianx@users.noreply.github.com>
2023-07-11 13:47:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
 	name: 'Aditya',
 	imageBackground: true,
 	openInNewTab: true,
-	twelveHourFormat: false,
+	twelveHourFormat: true,
 
 	// Greetings
 	greetingMorning: 'Good morning!',
